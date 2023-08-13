3990be93f0b1bb82e3715c43d73eaf01fe0248f6
stanislavsabev <bezraboten.34@gmail.com>
2023-08-13 04:53:22 +0200

    Resolve #4 | Add FxStr struct with string and length

---
+++ src/fxstring.c
@@ -0,0 +1,12 @@
+
+#include "fxstring.h"
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+int fxstr_trim(FxStr* s, FxStr* trimmed) {
+    (void)s;
+    (void)trimmed;
+    assert(0 && "Not implemented!");
+}
