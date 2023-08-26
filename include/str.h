f8b8219ed586b9e6bcc621fb85bb10f11c7bb80d
stanislavsabev <bezraboten.34@gmail.com>
2023-08-26 13:15:24 +0200

    Move literal macros to util.h

--- include/str.h
+++ include/str.h
@@ -8,8 +8,9 @@
  * @brief struct with len and char data[]
  */
 typedef struct fxstr_s {
+    size_t capacity;
     size_t len;
-    char data[];
+    char* data;
 } fxstr_t;
 
 /**
@@ -28,67 +29,26 @@ typedef struct fxstr_buf256_s {
     char data[256];
 } fxstr_buf256_t;
 
+#ifndef FX_NO_SHORT_NAMES
+
+#define str        fxstr_t
+#define str_buf64  fxstr_buf64_t
+#define str_buf256 fxstr_buf256_t
+#define str_null   fxstr_null
+
+#endif   // FX_NO_SHORT_NAMES
+
+#endif   // FX_FXSTR_H_
+
 // ------------------------------------------------------------------------------------------
 //  IMPLEMENTATION
 // ------------------------------------------------------------------------------------------
 #ifdef FX_IMPLEMENTATION
 
-#ifndef FX_NO_SHORT_NAMES
-
-#define DEFINE_TRIVIAL_CLEANUP_FUNC FX_DEFINE_TRIVIAL_CLEANUP_FUNC
-#define rept_literal                fx_rept_literal
-#define yes_no                      fx_yes_no
-#define true_false                  fx_true_false
-#define one_zero                    fx_one_zero
-#define str                         fxstr_t
-#define str_buf64                   fxstr_buf64_t
-#define str_buf256                  fxstr_buf256_t
-
-#else
-
-#endif   // FX_NO_SHORT_NAMES
-
-#define FX_DEFINE_TRIVIAL_CLEANUP_FUNC(type, func) \
-    static inline void func##p(type* p) {          \
-        if (*p) func(*p);                          \
-    }                                              \
-    struct __useless_struct_to_allow_trailing_semicolon__
-
-#define fx_rept_literal0(s)
-#define fx_rept_literal1(s)  s
-#define fx_rept_literal2(s)  fx_rept_literal1(s) s
-#define fx_rept_literal3(s)  fx_rept_literal2(s) s
-#define fx_rept_literal4(s)  fx_rept_literal3(s) s
-#define fx_rept_literal5(s)  fx_rept_literal4(s) s
-#define fx_rept_literal6(s)  fx_rept_literal5(s) s
-#define fx_rept_literal7(s)  fx_rept_literal6(s) s
-#define fx_rept_literal8(s)  fx_rept_literal7(s) s
-#define fx_rept_literal9(s)  fx_rept_literal8(s) s
-#define fx_rept_literal10(s) fx_rept_literal9(s) s
-
-/**
- * @brief Repeat string literal
- * @param hundreds number from 0 to 10
- * @param tens number from 0 to 10
- * @param ones number from 0 to 10
- * @param s string to repeat
- * @returns repeated string literal
- */
-#define fx_rept_literal(hundreds, tens, ones, s)                       \
-    fx_rept_literal##hundreds(fx_rept_literal10(fx_rept_literal10(s))) \
-        fx_rept_literal##tens(fx_rept_literal10(s)) fx_rept_literal##ones(s)
-
-static inline const char* fx_yes_no(bool b) {
-    return b ? "yes" : "no";
+fxstr_t fxstr_split_cstr(fxstr_t str, const char* delim, size_t n) {
 }
 
-static inline const char* fx_true_false(bool b) {
-    return b ? "true" : "false";
-}
-
-static inline const char* fx_one_zero(bool b) {
-    return b ? "1" : "0";
+char* fxstr_to_cstr(fxstr_t str) {
 }
 
 #endif   // FX_IMPLEMENTATION
-#endif   // FX_FXSTR_H_
