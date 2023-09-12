8feac0acd151302f5862c162e6131f717e67d081
stanislavsabev <bezraboten.34@gmail.com>
2023-09-13 00:00:10 +0200

    Update str.h and implement 3 str_buf functions
    str.h
      - rename all fxstr_view_s to str_view_s
      - rename all fxstr_buf_s to str_buf_s
      - rename all fxtr* types and variables to str*
      - rename all fxtr_ptr__ types and variables to str_p
      - rename all len_ variables to len
    str_view.c
      - rename variables to match str.h
      - rename all len_ variables to len
    str_buf.c
      - implement fxstr_buf_to_cstr
      - implement fxstr_buf_to_cstr_copy
      - implement fxstr_buf_to_str_view
      - update fxstr_null to assign 0 as capacity
      - rename variables to match str.h
      - rename all len_ variables to len

--- src/str_buf.c
+++ src/str_buf.c
@@ -17,37 +17,37 @@ static inline size_t _fxstr_compute_next_grow(size_t capacity_) {
 }
 
 /**
- * @brief For internal use. Grows the fxstr data.
- * @param fxstr_p__ pointer to the fxstr
+ * @brief For internal use. Grows the str data.
+ * @param str_p__ pointer to the str
  * @return void
  */
-void _fxstr_grow(str_buf_t* str_p_, size_t capacity_) {
-    const size_t ln__ = capacity_ * sizeof(*str_p_->data);
-    if (str_p_->data) {
-        void* data_p2__ = fxstr_stdlib_realloc(str_p_->data, ln__);
-        assert(data_p2__);
-        str_p_->data = data_p2__;
+void _fxstr_grow(str_buf_t* str_p__, size_t capacity_) {
+    const size_t ln__ = capacity_ * sizeof(*str_p__->data);
+    if (str_p__->data) {
+        void* data_p__ = fxstr_stdlib_realloc(str_p__->data, ln__);
+        assert(data_p__);
+        str_p__->data = data_p__;
     } else {
-        char* fxstr_p__ = fxstr_stdlib_malloc(ln__);
-        assert(fxstr_p__);
-        str_p_->data = fxstr_p__;
-        str_p_->len = 0;
+        char* data_p2__ = fxstr_stdlib_malloc(ln__);
+        assert(data_p2__);
+        str_p__->data = data_p2__;
+        str_p__->len = 0;
     }
-    str_p_->capacity = capacity_;
+    str_p__->capacity = capacity_;
 }
 
 extern inline str_buf_t fxstr_buf_null(void) {
-    return (str_buf_t){.capacity = _FXSTR_INITIAL_CAPACITY, .len = 0, .data = NULL};
+    return (str_buf_t){.capacity = 0, .len = 0, .data = NULL};
 }
 
-str_buf_t fxstr_buf_from_chars(size_t len_, const char chars_[static len_]) {
+str_buf_t fxstr_buf_from_chars(size_t len, const char chars_[static len]) {
     str_buf_t s__ = fxstr_buf_null();
-    if (len_ == 0) {
+    if (len == 0) {
         return s__;
     }
-    _fxstr_grow(&s__, len_);
+    _fxstr_grow(&s__, len);
     memcpy(s__.data, chars_, s__.len);
-    s__.len = len_;
+    s__.len = len;
     return s__;
 }
 
@@ -55,11 +55,23 @@ extern inline str_buf_t fxstr_buf_from_cstr(const char cstr[static 1]) {
     return fxstr_buf_from_chars(strlen(cstr), cstr);
 }
 
-char* fxstr_buf_to_cstr(const str_buf_t* str_ptr_) {
-    char* cstr = fxstr_stdlib_malloc(str_ptr_->len + 1);
-    memcpy(cstr, str_ptr_->data, str_ptr_->len);
-    cstr[str_ptr_->len] = '\0';
+char* fxstr_buf_to_cstr_copy(const str_buf_t* str_p) {
+    char* cstr = fxstr_stdlib_malloc(str_p->len + 1);
+    memcpy(cstr, str_p->data, str_p->len);
+    cstr[str_p->len] = '\0';
     return cstr;
+}
+
+char* fxstr_buf_to_cstr(str_buf_t* str_p) {
+    if (str_p->capacity < str_p->len + 1) {
+        _fxstr_grow(str_p, str_p->len + 1);
+    }
+    str_p->data[str_p->len] = '\0';
+    return str_p->data;
+}
+
+str_view_t fxstr_buf_to_str_view(const str_buf_t* str_p) {
+    return (str_view_t){.data = str_p->data, .len = str_p->len};
 }
 
 /**
@@ -67,8 +79,21 @@ char* fxstr_buf_to_cstr(const str_buf_t* str_ptr_) {
  * fxstr_stdlib_malloc', fxstr_stdlib_realloc' or `fxstr_stdlib_calloc'.
  * @return void
  */
-void fxstr_buf_free(str_buf_t* str_) {
-    if (str_->data) {
-        fxstr_stdlib_free(str_->data);
+void fxstr_buf_free(str_buf_t* str_p) {
+    if (str_p->data) {
+        fxstr_stdlib_free(str_p->data);
+    }
+}
+
+/**
+ * @brief Reserve an fxstr allocated by
+ * @param str_p pointer to the str
+ * @param str_p pointer to the str
+ * fxstr_stdlib_malloc', fxstr_stdlib_realloc' or `fxstr_stdlib_calloc'.
+ * @return void
+ */
+void fxstr_buf_reserve(str_buf_t* str_p, size_t capacity) {
+    if (str_p->capacity < capacity) {
+        _fxstr_grow(str_p, capacity);
     }
 }
--- src/str_view.c
+++ src/str_view.c
@@ -7,17 +7,17 @@ extern inline str_view_t fxstr_view_null(void) {
     return (str_view_t){.len = 0, .data = NULL};
 }
 
-extern inline str_view_t fxstr_view_from_chars(size_t len_, const char chars_[static len_]) {
-    return (str_view_t){.len = len_, .data = chars_};
+extern inline str_view_t fxstr_view_from_chars(size_t len, const char chars[static len]) {
+    return (str_view_t){.len = len, .data = chars};
 }
 
 extern inline str_view_t fxstr_view_from_cstr(const char cstr[static 1]) {
     return fxstr_view_from_chars(strlen(cstr), cstr);
 }
 
-char* fxstr_view_to_cstr(const str_view_t* fxstr_) {
-    char* cstr = fxstr_stdlib_malloc(fxstr_->len + 1);
-    memcpy(cstr, fxstr_->data, fxstr_->len);
-    cstr[fxstr_->len] = '\0';
+char* fxstr_view_to_cstr(const str_view_t* str_p) {
+    char* cstr = fxstr_stdlib_malloc(str_p->len + 1);
+    memcpy(cstr, str_p->data, str_p->len);
+    cstr[str_p->len] = '\0';
     return cstr;
 }
