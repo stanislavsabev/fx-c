43a69f167d53c4b7c2ba1314686ac51945ab2caa
stanislavsabev <bezraboten.34@gmail.com>
2026-01-30 07:56:09 +0100

    wip

--- fx.h
+++ fx.h
@@ -4,12 +4,14 @@
 #include <stdint.h>
 #include <stdlib.h>   // for malloc
 #include <string.h>   // for memset
+#include <stdbool.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 typedef int8_t i8;
+typedef int8_t s8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
@@ -32,37 +34,12 @@ typedef ptrdiff_t i64;
 #define enum32(type) u32
 #define enum64(type) u64
 
-#define NULL  ((void *)0)
-#define true  1
-#define false 0
 
 #ifndef MEM_DEFAULT_ALIGNMENT
 #define MEM_DEFAULT_ALIGNMENT (2 * sizeof(void *))
 #endif
 
-#ifndef offsetof
-#ifdef __GNUC__
-#define offsetof(type, member) __builtin_offsetof(type, member)
-#else
-#define offsetof(type, member) ((size_t)&((type *)0)->member)
-#endif
-#endif
-
-#if defined(__cplusplus)
-#define alignof(type) alignof(type)
-#elif defined(__GNUC__) || defined(__clang__)
 #define alignof(type) __alignof__(type)
-#elif defined(_MSC_VER)
-#define alignof(type) __alignof(type)
-#else
-#define alignof(type)    \
-    offsetof(            \
-        struct {         \
-            char c;      \
-            type member; \
-        },               \
-        member)
-#endif
 
 #define mem_zero(ptr, size) memset((ptr), 0, (size))
 
@@ -72,8 +49,8 @@ typedef struct String {
 } String;
 
 typedef struct Arena {
-    u8 *data;
-    u64 capacity;
+    u8 *buffer;
+    u64 buffer_size;
     u64 offset;
 } Arena;
 
@@ -82,20 +59,16 @@ typedef struct ArenaSnapshot {
     u64 offset;        // The offset at the time of the snapshot
 } ArenaSnapshot;
 
-// #define fn
-// fn b8 mem_is_power_of_two(u64 value);
-// fn u64 mem_align_forward(u64 value, u64 alignment);
-// fn void arena_init(Arena *arena, void *buffer, u64 capacity);
+#define fn
 
-// fn void *arena_alloc_align(Arena *arena, u64 size, u64 alignment, b32 set_zero);
-// #define arena_alloc(arena, size)        arena_alloc_align(arena, size, MEM_DEFAULT_ALIGNMENT, true)
-// #define arena_alloc_struct(arena, type) arena_alloc_align(arena, sizeof(type), alignof(type), true)
-// #define arena_alloc_array(arena, count, type) \
-//     arena_alloc_align(arena, (count) * sizeof(type), alignof(type[1]))
-
-// fn void arena_free(Arena *arena);
-// fn void arena_reset(Arena *arena);
-#endif   // __FX_LIB__
+b8 mem_is_power_of_two(u64 value);
+u64 mem_align_forward(u64 ptr, u64 alignment);
+void arena_init(Arena *a, void *buffer, u64 capacity);
+void *arena_alloc_align(Arena *arena, u64 size, u64 alignment, b32 set_zero);
+void arena_free(Arena *arena);
+void arena_reset(Arena *arena);
+ArenaSnapshot arena_snapshot_save(Arena *arena);
+void arena_snapshot_restore(ArenaSnapshot snapshot);
 
 #ifdef FX_IMPLEMENTATION
 
@@ -113,7 +86,7 @@ fn u64 mem_align_forward(u64 ptr, u64 alignment) {
     u64 p, a, modulo;
     p = ptr;
     a = alignment;
-    modulo = p % (a - 1);
+    modulo = p % (a - 1); 
     if (modulo != 0) {
         p += a - modulo;
     }
@@ -121,21 +94,21 @@ fn u64 mem_align_forward(u64 ptr, u64 alignment) {
 }
 
 fn void arena_init(Arena *a, void *buffer, u64 capacity) {
-    a->data = (u8 *)buffer;
-    a->capacity = capacity;
+    a->buffer = (u8 *)buffer;
+    a->buffer_size = capacity;
     a->offset = 0;
 }
 
 fn void *arena_alloc_align(Arena *arena, u64 size, u64 alignment, b32 set_zero) {
-    up64 curr_ptr = (up64)(arena->data + arena->offset);
+    up64 curr_ptr = (up64)(arena->buffer + arena->offset);
     up64 offset = mem_align_forward(curr_ptr, alignment);
-    offset -= (up64)arena->data;   // Adjust offset to be relative to arena->data
+    offset -= (up64)arena->buffer;   // Adjust offset to be relative to arena->data
 
-    if (offset + size > arena->capacity) {
+    if (offset + size > arena->buffer_size) {
         return NULL;   // Not enough space in the arena
     }
 
-    void *ptr = &arena->data[offset];
+    void *ptr = &arena->buffer[offset];
     arena->offset = offset + size;
 
     if (set_zero) {
@@ -144,10 +117,19 @@ fn void *arena_alloc_align(Arena *arena, u64 size, u64 alignment, b32 set_zero) 
     return ptr;
 }
 
-fn void arena_free(Arena *arena) {
-    // No-op, as we don't manage heap memory in the arena
+fn void *arena_alloc(Arena *arena, u64 size) {
+    return arena_alloc_align(arena, size, MEM_DEFAULT_ALIGNMENT, true);
 }
 
+fn void arena_free(Arena *arena) {
+    // No-op, as we don't manage heap memory in the arena
+    (void)arena;
+}
+
+// Get remaining space in arena
+size_t arena_remaining(Arena* arena) {
+    return arena->buffer_size - arena->offset;
+}
 fn void arena_reset(Arena *arena) {
     arena->offset = 0;
 }
@@ -166,3 +148,4 @@ fn void arena_snapshot_restore(ArenaSnapshot snapshot) {
 
 
 #endif   // FX_IMPLEMENTATION
+#endif   // __FX_LIB__
--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #define FX_IMPLEMENTATION
 #include "fx.h"
+#include <stdio.h>
+#include <assert.h>
 
 
 int main() {
 
+    u32 ARENA_CAPACITY = 256;
+    s8 buffer[ARENA_CAPACITY];
+    Arena a = {0};
+    arena_init(&a, buffer, ARENA_CAPACITY);
+
+    i32 *ints = (i32*)arena_alloc(&a, sizeof(i32) * 100);
+    i32 *p = ints;
+
+    for(usize x = 0; x < 100; ++x){
+        i32 v = *p++;
+        v *= 2;
+        printf("v=%d", v);
+    }
     return 0;
 }
