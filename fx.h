6179450a92cdc7e28fc45ec5e9cbeb63b2d715a2
stanislavsabev <bezraboten.34@gmail.com>
2026-01-31 21:27:43 +0100

    Finished linear arena impl

--- fx.h
+++ fx.h
@@ -1,17 +1,16 @@
 #ifndef __FX_LIB__
 #define __FX_LIB__
+#include <stdbool.h>
 #include <stddef.h>   // for NULL
 #include <stdint.h>
 #include <stdlib.h>   // for malloc
 #include <string.h>   // for memset
-#include <stdbool.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 typedef int8_t i8;
-typedef int8_t s8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
@@ -27,16 +26,15 @@ typedef wchar_t wchar;
 typedef uintptr_t up64;
 typedef intptr_t p64;
 typedef size_t usize;
-typedef ptrdiff_t i64;
+typedef ptrdiff_t pdiff;
 
 #define enum8(type)  u8
 #define enum16(type) u16
 #define enum32(type) u32
 #define enum64(type) u64
 
-
 #ifndef MEM_DEFAULT_ALIGNMENT
-#define MEM_DEFAULT_ALIGNMENT (2 * sizeof(void *))
+#define MEM_DEFAULT_ALIGNMENT (2 * sizeof(void*))
 #endif
 
 #define alignof(type) __alignof__(type)
@@ -44,34 +42,33 @@ typedef ptrdiff_t i64;
 #define mem_zero(ptr, size) memset((ptr), 0, (size))
 
 typedef struct String {
-    u8 *data;
+    u8* data;
     u64 len;
 } String;
 
 typedef struct Arena {
-    u8 *buffer;
+    u8* buffer;
     u64 buffer_size;
     u64 offset;
 } Arena;
 
 typedef struct ArenaSnapshot {
-    Arena *arena;
-    u64 offset;        // The offset at the time of the snapshot
+    Arena* arena;
+    u64 offset;   // The offset at the time of the snapshot
 } ArenaSnapshot;
 
 #define fn
 
 b8 mem_is_power_of_two(u64 value);
 u64 mem_align_forward(u64 ptr, u64 alignment);
-void arena_init(Arena *a, void *buffer, u64 capacity);
-void *arena_alloc_align(Arena *arena, u64 size, u64 alignment, b32 set_zero);
-void arena_free(Arena *arena);
-void arena_reset(Arena *arena);
-ArenaSnapshot arena_snapshot_save(Arena *arena);
+void arena_init(Arena* a, void* buffer, u64 capacity);
+void* arena_alloc_align(Arena* arena, u64 size, u64 alignment, b32 set_zero);
+void arena_free(Arena* arena);
+void arena_reset(Arena* arena);
+ArenaSnapshot arena_snapshot_save(Arena* arena);
 void arena_snapshot_restore(ArenaSnapshot snapshot);
 
 #ifdef FX_IMPLEMENTATION
-
 
 fn b8 mem_is_power_of_two(u64 value) {
     return (value & (value - 1)) == 0;
@@ -79,27 +76,26 @@ fn b8 mem_is_power_of_two(u64 value) {
 
 #include <assert.h>
 
-
 fn u64 mem_align_forward(u64 ptr, u64 alignment) {
     assert(mem_is_power_of_two(alignment) && "Alignment must be a power of two");
 
     u64 p, a, modulo;
     p = ptr;
     a = alignment;
-    modulo = p % (a - 1); 
+    modulo = p & (a - 1);
     if (modulo != 0) {
         p += a - modulo;
     }
     return p;
 }
 
-fn void arena_init(Arena *a, void *buffer, u64 capacity) {
-    a->buffer = (u8 *)buffer;
+fn void arena_init(Arena* a, void* buffer, u64 capacity) {
+    a->buffer = (u8*)buffer;
     a->buffer_size = capacity;
     a->offset = 0;
 }
 
-fn void *arena_alloc_align(Arena *arena, u64 size, u64 alignment, b32 set_zero) {
+fn void* arena_alloc_align(Arena* arena, u64 size, u64 alignment, b32 set_zero) {
     up64 curr_ptr = (up64)(arena->buffer + arena->offset);
     up64 offset = mem_align_forward(curr_ptr, alignment);
     offset -= (up64)arena->buffer;   // Adjust offset to be relative to arena->data
@@ -108,7 +104,7 @@ fn void *arena_alloc_align(Arena *arena, u64 size, u64 alignment, b32 set_zero) 
         return NULL;   // Not enough space in the arena
     }
 
-    void *ptr = &arena->buffer[offset];
+    void* ptr = &arena->buffer[offset];
     arena->offset = offset + size;
 
     if (set_zero) {
@@ -117,11 +113,11 @@ fn void *arena_alloc_align(Arena *arena, u64 size, u64 alignment, b32 set_zero) 
     return ptr;
 }
 
-fn void *arena_alloc(Arena *arena, u64 size) {
+fn void* arena_alloc(Arena* arena, u64 size) {
     return arena_alloc_align(arena, size, MEM_DEFAULT_ALIGNMENT, true);
 }
 
-fn void arena_free(Arena *arena) {
+fn void arena_free(Arena* arena) {
     // No-op, as we don't manage heap memory in the arena
     (void)arena;
 }
@@ -130,11 +126,11 @@ fn void arena_free(Arena *arena) {
 size_t arena_remaining(Arena* arena) {
     return arena->buffer_size - arena->offset;
 }
-fn void arena_reset(Arena *arena) {
+fn void arena_reset(Arena* arena) {
     arena->offset = 0;
 }
 
-fn ArenaSnapshot arena_snapshot_save(Arena *arena) {
+fn ArenaSnapshot arena_snapshot_save(Arena* arena) {
     ArenaSnapshot snapshot;
     snapshot.arena = arena;
     snapshot.offset = arena->offset;
@@ -142,10 +138,9 @@ fn ArenaSnapshot arena_snapshot_save(Arena *arena) {
 }
 
 fn void arena_snapshot_restore(ArenaSnapshot snapshot) {
-    Arena *arena = snapshot.arena;
+    Arena* arena = snapshot.arena;
     arena->offset = snapshot.offset;
 }
-
 
 #endif   // FX_IMPLEMENTATION
 #endif   // __FX_LIB__
--- main.c
+++ main.c
@@ -1,23 +1,53 @@
 #define FX_IMPLEMENTATION
-#include "fx.h"
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
+#include "fx.h"
+
+typedef struct ByteArray {
+    usize len;
+    char* data;
+} ByteArray;
 
 int main() {
-
     u32 ARENA_CAPACITY = 256;
-    s8 buffer[ARENA_CAPACITY];
+    i8 buffer[ARENA_CAPACITY];
     Arena a = {0};
     arena_init(&a, buffer, ARENA_CAPACITY);
 
-    i32 *ints = (i32*)arena_alloc(&a, sizeof(i32) * 100);
-    i32 *p = ints;
+    usize len = 100;
 
-    for(usize x = 0; x < 100; ++x){
-        i32 v = *p++;
-        v *= 2;
-        printf("v=%d", v);
+    // char *chars = (char *)arena_alloc(&a, sizeof(char) * len);
+    ByteArray* barr = (ByteArray*)arena_alloc(&a, sizeof(ByteArray) + sizeof(char) * len);
+    barr->data = (char*)arena_alloc(&a, sizeof(char) + sizeof(char) * len);
+
+    barr->len = len;
+    memset(barr->data, 'a', barr->len);
+
+    for (usize x = 0; x < barr->len; ++x) {
+        char p = barr->data[x];
+        char* py = barr->data + x;
+        printf("%c:%c ", p, *py);
     }
+
+    // for(usize x = 0; x < len; ++x){
+    //     char v = *(chars + x);
+    //     printf("%c ", v);
+    // }
+    printf("\n");
+
+    memset(barr->data, 'b', barr->len);
+    // memset(chars, 'b', len);
+
+    for (usize x = 0; x < barr->len; ++x) {
+        char y = barr->data[x];
+        printf("%c ", y);
+    }
+    // for(usize x = 0; x < len; ++x){
+    //     char v = *(chars + x);
+    //     printf("%c ", v);
+    // }
+    printf("\n");
+
     return 0;
 }
